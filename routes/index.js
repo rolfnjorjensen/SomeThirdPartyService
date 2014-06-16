6376340c72eee816d7c02a83f86efc4d4d3102bb
Rolf Njor Jensen <rnj@tradeshift.com>
2014-06-16 00:22:29 -0700

    New App

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.redirect('https://api-apps-sandbox.tradeshift.com/tradeshift/auth/login?response_type=code&client_id=AcmeCorp.DemoService&redirect_uri=https://somethirdpartyservice.heroku.com/service&scope=openid');
+  res.redirect('https://api-apps-sandbox.tradeshift.com/tradeshift/auth/login?response_type=code&client_id=AcmeCorp.DemoService2&redirect_uri=https://somethirdpartyservice.heroku.com/service&scope=openid');
 });
 
 module.exports = router;
